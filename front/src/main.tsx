f38ba45b6e2886124392875357ecef39d1d5ea7c
Ekalape <ekalape@outlook.it>
2023-09-04 07:11:55 +0200

    feat: add routes and couple of icons

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -1,10 +1,28 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { AddPage } from './pages/add_page/index.tsx';
+import { StatsPage } from './pages/stats_page/index.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/add',
+    element: <AddPage />,
+  },
+  {
+    path: '/stats',
+    element: <StatsPage />,
+  },
+  {
+    path: '/',
+    element: <App />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
-)
+);
